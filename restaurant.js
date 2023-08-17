d6081ff763cceac8e78121be6c5fc2852a87d6f1
Barshon <barshonweb@gmail.com>
2023-08-17 12:18:33 +0600

    dynamic searched meals added

--- restaurant.js
+++ restaurant.js
@@ -1,7 +1,9 @@
 // meal load function 
 
 const searchMeal=()=>{
-    
+    const searched= document.getElementById("search");
+    const searchResult=searched.value;
+    loadSearchedMeal(searchResult);
 }
 
 const loadMeal=async()=>{
@@ -9,7 +11,6 @@ const loadMeal=async()=>{
         const url= "https://www.themealdb.com/api/json/v1/1/categories.php";
         const res= await fetch(url);
         const data= await res.json();
-        console.log(data.categories);
         displayMeal(data.categories);
     }
 
@@ -18,6 +19,19 @@ const loadMeal=async()=>{
     }
 }
 
+const loadSearchedMeal= async(searchResult)=>{
+    try {
+        const url= `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchResult}`
+        const res= await fetch(url);
+        const data= await res.json();
+        console.log(data.meals);
+        displaySearchedMeal(data.meals)
+    }
+
+    catch (error){
+        console.log(error);
+    }
+}
 const displayMeal=(meals)=>{
     const container=document.getElementById("menu");
     container.innerText="";
@@ -38,5 +52,26 @@ const displayMeal=(meals)=>{
           container.appendChild(mealCard);
     })
 }
+
+const displaySearchedMeal = (meals) => {
+  const container = document.getElementById("menu");
+  container.innerText = "";
+  meals.forEach((meal) => {
+    const mealCard = document.createElement("div");
+    mealCard.classList.add("col");
+    mealCard.innerHTML = `<div class="col">
+            <div class="card">
+              <img src="${meal.strCategoryThumb}" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title"> ${meal.strCategory}</h5>
+                <p class="card-text">
+                  ${meal.strCategoryDescription}
+                </p>
+              </div>
+            </div>
+          </div>`;
+    container.appendChild(mealCard);
+  });
+};
 
 loadMeal();
