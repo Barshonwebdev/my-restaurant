8ad2e67d5c6f5ed0c3b8cbb808e260e551ba2d19
Barshon <barshonweb@gmail.com>
2023-08-17 11:23:42 +0600

    dynamic category generated

--- restaurant.js
+++ restaurant.js
@@ -10,12 +10,33 @@ const loadMeal=async()=>{
         const res= await fetch(url);
         const data= await res.json();
         console.log(data.categories);
-        displayMeal();
+        displayMeal(data.categories);
     }
 
     catch (error){
         console.log(error);
     }
+}
+
+const displayMeal=(meals)=>{
+    const container=document.getElementById("menu");
+    container.innerText="";
+    meals.forEach(meal=>{
+        const mealCard=document.createElement("div");
+        mealCard.classList.add("col");
+        mealCard.innerHTML = `<div class="col">
+            <div class="card">
+              <img src="${meal.strCategoryThumb}" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title"> ${meal.strCategory}</h5>
+                <p class="card-text">
+                  ${meal.strCategoryDescription}
+                </p>
+              </div>
+            </div>
+          </div>`;
+          container.appendChild(mealCard);
+    })
 }
 
 loadMeal();
